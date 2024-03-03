dfe7ee6bf2845735bf5446aa2022dca9b1f8b743
Delly <daehyeon1999@naver.com>
2024-03-03 11:42:27 +0900

    format: 🎨디펜던시 정리

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,6 @@ val kotlin_version: String by project
 val logback_version: String by project
 
 val exposed_version: String by project
-val h2_version: String by project
-val postgres_version: String by project
 plugins {
   kotlin("jvm") version "1.9.22"
   id("io.ktor.plugin") version "2.3.8"
@@ -27,19 +25,28 @@ repositories {
 
 dependencies {
 
-  // Ktor Server
-  implementation("io.ktor:ktor-server-core-jvm")
-  implementation("io.ktor:ktor-server-auth-jvm")
-  implementation("io.ktor:ktor-server-auth-jwt-jvm")
-  implementation("io.ktor:ktor-server-host-common-jvm")
-  implementation("io.ktor:ktor-server-resources")
-  implementation("io.ktor:ktor-server-status-pages-jvm")
-  implementation("io.ktor:ktor-server-swagger-jvm")
-  implementation("io.ktor:ktor-server-content-negotiation-jvm")
-  implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-  implementation("io.ktor:ktor-server-netty-jvm")
-  implementation("io.ktor:ktor-server-config-yaml:2.3.8")
-  implementation("io.ktor:ktor-serialization-kotlinx-protobuf:$ktor_version")
+  listOf(
+    "core-jvm",
+    "auth-jvm",
+    "auth-jwt-jvm",
+    "host-common-jvm",
+    "resources",
+    "status-pages-jvm",
+    "swagger-jvm",
+    "content-negotiation-jvm",
+    "serialization-kotlinx-json-jvm",
+    "server-netty-jvm",
+    "server-config-yaml"
+  ).forEach {
+    implementation("io.ktor:ktor-server-$it")
+  }
+
+  listOf(
+    "serialization-kotlinx-protobuf",
+    "serialization-kotlinx-json-jvm"
+  ).forEach {
+    implementation("io.ktor:ktor-$it:$ktor_version")
+  }
 
   // Ktor Client
   implementation("io.ktor:ktor-client-core-jvm")
@@ -49,8 +56,12 @@ dependencies {
   // database
   implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
   implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-  implementation("com.h2database:h2:$h2_version")
-  implementation("org.postgresql:postgresql:$postgres_version")
+
+  listOf("exposed-core", "exposed-dao", "exposed-jdbc", "exposed-java-time").forEach {
+    implementation("org.jetbrains.exposed:exposed-$it:$exposed_version")
+  }
+
+  implementation("org.postgresql:postgresql:42.2")
 
   // logging
   implementation("ch.qos.logback:logback-classic:$logback_version")
