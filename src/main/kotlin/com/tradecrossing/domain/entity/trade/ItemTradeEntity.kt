fc9a61d6598c7a25ab47f84b36156c82a92fe8f8
Delly <daehyeon1999@naver.com>
2024-03-19 23:15:08 +0900

    feat: 🏷️ Entity 추가
    - ItemTradeEntity 추가

---
+++ src/main/kotlin/com/tradecrossing/domain/entity/trade/ItemTradeEntity.kt
@@ -0,0 +1,34 @@
+package com.tradecrossing.domain.entity.trade
+
+import com.tradecrossing.domain.entity.resident.ResidentEntity
+import com.tradecrossing.domain.tables.trade.ItemTradeTable
+import org.jetbrains.exposed.dao.id.EntityID
+import java.time.LocalDateTime
+
+class ItemTradeEntity(id: EntityID<Long>) : BaseTradeEntity(id) {
+  override val resident: ResidentEntity by ResidentEntity referencedOn ItemTradeTable.resident
+  override var closed: Boolean by ItemTradeTable.closed
+  override var bellPrice: Int? by ItemTradeTable.bellPrice
+  override var milePrice: Int? by ItemTradeTable.milePrice
+  override var isDeleted: Boolean by ItemTradeTable.isDeleted
+  override var createdAt: LocalDateTime by ItemTradeTable.createdAt
+  override var updatedAt: LocalDateTime by ItemTradeTable.updatedAt
+  var tradeType by ItemTradeTable.tradeType
+  val category by ItemCategoryEntity referencedOn ItemTradeTable.itemCategory
+  val source by SourceEntity referencedOn ItemTradeTable.itemSource
+
+  override fun toString(): String {
+    return "ItemTradeEntity(" +
+        "id=$id, " +
+        "resident=$resident, " +
+        "closed=$closed, " +
+        "bellPrice=$bellPrice, " +
+        "milePrice=$milePrice, " +
+        "isDeleted=$isDeleted, " +
+        "createdAt=$createdAt, " +
+        "updatedAt=$updatedAt, " +
+        "tradeType=$tradeType, " +
+        "category=$category, " +
+        "source=$source)"
+  }
+}
