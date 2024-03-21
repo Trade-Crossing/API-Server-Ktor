480b571743d317c0fd189a2f7a34f4ca3b3125f1
Delly <daehyeon1999@naver.com>
2024-03-21 10:52:15 +0900

    format: 🎨 OAuth API의 API 문서 수정

--- src/main/kotlin/com/tradecrossing/api/auth/AuthRouting.kt
+++ src/main/kotlin/com/tradecrossing/api/auth/AuthRouting.kt
@@ -2,13 +2,10 @@ package com.tradecrossing.api.auth
 
 
 import com.tradecrossing.dto.request.auth.RegisterRequest
-import com.tradecrossing.dto.response.ErrorResponse
-import com.tradecrossing.dto.response.resident.ResidentInfoDto
 import com.tradecrossing.service.AuthService
 import com.tradecrossing.system.plugins.getUserId
 import com.tradecrossing.system.plugins.withAuth
 import com.tradecrossing.types.TokenType
-import io.github.smiley4.ktorswaggerui.dsl.OpenApiRoute
 import io.github.smiley4.ktorswaggerui.dsl.resources.get
 import io.github.smiley4.ktorswaggerui.dsl.resources.post
 import io.ktor.http.*
@@ -22,7 +19,7 @@ fun Route.auth() {
   val authService by inject<AuthService>(AuthService::class.java)
 
   withAuth(TokenType.ACCESS) {
-    post<AuthResource.Register>(register) {
+    post<AuthResource.Register>(AuthResource.Register.post) {
       val body = call.receive<RegisterRequest>()
       val id = call.getUserId()
 
@@ -30,73 +27,17 @@ fun Route.auth() {
       call.respond(HttpStatusCode.Created, "성공")
     }
 
-    get<AuthResource.Info>(info) {
+    get<AuthResource.Info>(AuthResource.Info.get) {
       val id = call.getUserId()
       val response = authService.getResidentInfo(id)
 
       call.respond(response)
     }
+
+
   }
 
   withAuth(TokenType.REFRESH) {
     post<AuthResource.Refresh> {}
-  }
-}
-
-
-private val register: OpenApiRoute.() -> Unit = {
-  tags = listOf("Auth")
-  summary = "회원가입"
-  description = "회원가입을 합니다."
-  request {
-    body<RegisterRequest>()
-  }
-
-  response {
-    // 409
-    HttpStatusCode.Conflict to {
-      description = "이미 가입된 회원입니다."
-      body<ErrorResponse> {
-        example("conflict", ErrorResponse("conflict", "이미 가입된 회원입니다."))
-      }
-    }
-
-    // 404
-    HttpStatusCode.NotFound to {
-      description = "존재하지 않는 "
-      body<ErrorResponse> {
-        example("not_found", ErrorResponse("not_found", "회원가입에 실패했습니다."))
-      }
-    }
-
-  }
-}
-
-private val info: OpenApiRoute.() -> Unit = {
-  tags = listOf("Auth")
-  summary = "회원정보"
-  description = "회원정보를 가져옵니다."
-  securitySchemeName = "Jwt"
-  request {
-    headerParameter<String>("Authorization") {
-      description = "Bearer Token"
-      required = true
-    }
-  }
-  response {
-    HttpStatusCode.OK to {
-      body<ResidentInfoDto>()
-      description = "성공"
-    }
-
-    HttpStatusCode.NotFound to {
-      body<ErrorResponse>()
-      description = "존재하지 않는 유저입니다."
-    }
-
-    HttpStatusCode.Unauthorized to {
-      body<ErrorResponse>()
-      description = "인증에 실패했습니다."
-    }
   }
 }
--- src/main/kotlin/com/tradecrossing/api/oauth/OAuthRouting.kt
+++ src/main/kotlin/com/tradecrossing/api/oauth/OAuthRouting.kt
@@ -1,12 +1,10 @@
 package com.tradecrossing.api.oauth
 
 import com.tradecrossing.dto.request.oauth.MobileLoginRequest
-import com.tradecrossing.dto.response.ErrorResponse
 import com.tradecrossing.dto.response.oauth.OAuthResponse
 import com.tradecrossing.service.OAuthService
 import com.tradecrossing.system.plugins.generateJwtToken
 import com.tradecrossing.types.OAuthProvider
-import io.github.smiley4.ktorswaggerui.dsl.OpenApiRoute
 import io.github.smiley4.ktorswaggerui.dsl.resources.get
 import io.github.smiley4.ktorswaggerui.dsl.resources.post
 import io.ktor.http.*
@@ -21,11 +19,10 @@ fun Route.oauth() {
   val service by inject<OAuthService>(OAuthService::class.java)
 
   authenticate("google") {
-
     // Google OAuth2 실행하는 API
-    get<OAuthResource.Google>(google) {}
+    get<OAuthResource.Google>(OAuthResource.Google.googleLogin) {}
 
-    get<OAuthResource.Google.CallBack>(googleCallBack) {
+    get<OAuthResource.Google.CallBack>(OAuthResource.Google.googleCallBack) {
       val principal: OAuthAccessTokenResponse.OAuth2? = call.principal()
       val accessToken = principal?.accessToken!!
       val result = service.getGoogleUserInfo(accessToken)
@@ -38,9 +35,9 @@ fun Route.oauth() {
   }
 
   authenticate("kakao") {
-    get<OAuthResource.Kakao>(kakao) {}
+    get<OAuthResource.Kakao>(OAuthResource.Kakao.kakao) {}
 
-    get<OAuthResource.Kakao.CallBack>(kakaoCallBack) {
+    get<OAuthResource.Kakao.CallBack>(OAuthResource.Kakao.kakaoCallBack) {
       val principal: OAuthAccessTokenResponse.OAuth2? = call.principal()
       val accessToken = principal?.accessToken!!
       val result = service.getKakaoUserInfo(accessToken)
@@ -56,67 +53,12 @@ fun Route.oauth() {
     }
   }
 
-  post<OAuthResource.Mobile>(mobile) {
+  post<OAuthResource.Mobile>(OAuthResource.Mobile.post) {
     val body = call.receive<MobileLoginRequest>()
     val resident = service.mobileLogin(body)
     val token = application.generateJwtToken(resident.id)
     val response = OAuthResponse(resident.registered, null, token.accessToken, token.refreshToken)
 
     call.respond(HttpStatusCode.OK, response)
-  }
-}
-
-
-private val google: OpenApiRoute.() -> Unit = {
-  tags = listOf("OAuth")
-  summary = "Google OAuth2 실행하는 API"
-}
-
-private val googleCallBack: OpenApiRoute.() -> Unit = {
-  tags = listOf("OAuth")
-  summary = "Google OAuth 콜백"
-  response {
-    HttpStatusCode.OK to {
-      body<OAuthResponse>()
-      description = "성공"
-    }
-  }
-}
-
-private val kakao: OpenApiRoute.() -> Unit = {
-  tags = listOf("OAuth")
-  summary = "Kakao OAuth 실행하는 API"
-}
-
-private val kakaoCallBack: OpenApiRoute.() -> Unit = {
-  tags = listOf("OAuth")
-  summary = "Kakao OAuth 콜백"
-  response {
-    HttpStatusCode.OK to {
-      body<OAuthResponse>()
-      description = "성공"
-    }
-  }
-}
-
-private val mobile: OpenApiRoute.() -> Unit = {
-  tags = listOf("OAuth")
-  summary = "모바일 로그인"
-  request {
-    body<MobileLoginRequest> {
-      example("1", MobileLoginRequest("12345", "email@email.com", provider = OAuthProvider.google))
-      example("2", MobileLoginRequest("12345", "email@email.com", provider = OAuthProvider.kakao))
-    }
-  }
-  response {
-    HttpStatusCode.OK to {
-      body<OAuthResponse>()
-      description = "성공"
-    }
-
-    HttpStatusCode.NotFound to {
-      body<ErrorResponse>()
-      description = "등록되지 않은 사용자입니다."
-    }
   }
 }
