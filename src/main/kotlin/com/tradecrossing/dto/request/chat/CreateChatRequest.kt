809a5124be39ed98df8de43684ee238a017e95f7
Delly <daehyeon1999@naver.com>
2024-06-09 21:14:10 +0900

    Add: 🏷️ 채팅방 생성 DTO 개발
    - 새로운 채팅방을 생성하는 DTO 개발

---
+++ src/main/kotlin/com/tradecrossing/dto/request/chat/CreateChatRequest.kt
@@ -0,0 +1,13 @@
+package com.tradecrossing.dto.request.chat
+
+import com.tradecrossing.types.UUIDSerializer
+import io.swagger.v3.oas.annotations.media.Schema
+import kotlinx.serialization.Serializable
+import java.util.UUID
+
+@Serializable
+data class CreateChatRequest(
+  @Serializable(with = UUIDSerializer::class)
+  @field:Schema(implementation = UUID::class, description = "상대방 id")
+  val residentId:UUID
+)
