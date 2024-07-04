1c9abcfa9c94b7515255afa5f44644d4da4eb59a
Delly <daehyeon1999@naver.com>
2024-07-04 11:51:53 +0900

    Add: 🏷️ Report 테이블 추가

--- src/main/kotlin/com/tradecrossing/domain/Reports.kt
+++ src/main/kotlin/com/tradecrossing/domain/Reports.kt
@@ -1,19 +1,21 @@
 package com.tradecrossing.domain
 
-import com.tradecrossing.domain.entity.resident.ResidentEntity
-import com.tradecrossing.domain.tables.resident.ResidentTable
 import com.tradecrossing.types.ReportStatus
 import com.tradecrossing.types.TradeCategory
+import com.tradecrossing.types.UUIDSerializer
+import io.swagger.v3.oas.annotations.media.Schema
+import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.dao.LongEntity
 import org.jetbrains.exposed.dao.LongEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.LongIdTable
 import org.jetbrains.exposed.sql.javatime.datetime
 import java.time.LocalDateTime
+import java.util.*
 
 
 object Reports : LongIdTable("reports") {
-  val reporterId = reference("reporter_id", ResidentTable.id)
+  val reporterId = reference("reporter_id", Residents.id)
   val tradeId = long("trade_id")
   val tradeCategory = enumeration<TradeCategory>("trade_category")
   val reason = text("reason")
@@ -22,10 +24,10 @@ object Reports : LongIdTable("reports") {
   val status = enumeration<ReportStatus>("status").default(ReportStatus.pending)
 }
 
-class ReportEntity(id: EntityID<Long>) : LongEntity(id) {
-  companion object : LongEntityClass<ReportEntity>(Reports)
+class Report(id: EntityID<Long>) : LongEntity(id) {
+  companion object : LongEntityClass<Report>(Reports)
 
-  var reporter by ResidentEntity referencedOn Reports.reporterId
+  var reporter by Resident referencedOn Reports.reporterId
   var repoterId by Reports.reporterId
   var tradeId by Reports.tradeId
   var tradeCategory by Reports.tradeCategory
@@ -33,4 +35,19 @@ class ReportEntity(id: EntityID<Long>) : LongEntity(id) {
   val reportDate by Reports.reportDate
   var resolvedTime by Reports.resolvedTime
   var status by Reports.status
+
+
+  @Serializable
+  @Schema(name = "Report Request")
+  data class Request(
+    @Serializable(with = UUIDSerializer::class)
+    @field:Schema(description = "신고자", required = true)
+    val reporterId: UUID,
+    @field:Schema(description = "신고 대상 거래 ID", required = true)
+    val tradeId: Long,
+    @field:Schema(description = "신고 대상 거래 카테고리", required = true, defaultValue = "item")
+    val tradeCategory: TradeCategory,
+    @field:Schema(description = "신고 사유", required = true)
+    val reason: String
+  )
 }
