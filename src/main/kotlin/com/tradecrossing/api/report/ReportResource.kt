9f5ce865a00658b80e8fa0bdaf0472fd12657aa0
Delly <daehyeon1999@naver.com>
2024-07-04 11:53:37 +0900

    Add: ✨Report 라우팅 추가 및 Swagger 문서 작성

---
+++ src/main/kotlin/com/tradecrossing/api/report/ReportResource.kt
@@ -0,0 +1,34 @@
+package com.tradecrossing.api.report
+
+import com.tradecrossing.domain.Report
+import io.github.smiley4.ktorswaggerui.dsl.OpenApiRoute
+import io.ktor.http.*
+import io.ktor.resources.*
+
+@Resource("/report")
+class ReportResource {
+
+  companion object {
+    val post: OpenApiRoute.() -> Unit = {
+      tags = listOf("Report")
+      summary = "신고하기"
+      securitySchemeName = "Jwt"
+      protected = true
+      request {
+        body<Report.Request>()
+      }
+
+      response {
+        HttpStatusCode.OK to {
+          description = "신고 성공"
+        }
+        HttpStatusCode.NotFound to {
+          description = "존재하지 않는 유저입니다."
+        }
+        HttpStatusCode.Unauthorized to {
+          description = "인증되지 않았습니다. 필요합니다."
+        }
+      }
+    }
+  }
+}
---
+++ src/main/kotlin/com/tradecrossing/api/report/ReportRouting.kt
@@ -0,0 +1,21 @@
+package com.tradecrossing.api.report
+
+import com.tradecrossing.domain.Report
+import com.tradecrossing.system.plugins.getUserId
+import com.tradecrossing.system.plugins.withAuth
+import com.tradecrossing.types.TokenType
+import io.github.smiley4.ktorswaggerui.dsl.resources.post
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.routing.*
+
+fun Route.report() {
+
+
+  withAuth(TokenType.ACCESS) {
+    post<ReportResource>(ReportResource.post) {
+      val userId = call.getUserId()
+      val body = call.receive<Report.Request>()
+    }
+  }
+}
